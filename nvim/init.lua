76ea1a0158f3f019e614e0b8773c1e9ecb181141
Dawid <dawid_03@onet.eu>
2026-03-02 19:51:31 +0100

    add cursor animation

--- nvim/init.lua
+++ nvim/init.lua
@@ -329,6 +329,27 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>dx", dap.terminate, { desc = "Stop Debugging" })
 		end,
 	},
+	--cursor animation
+	{
+		"sphamba/smear-cursor.nvim",
+
+		opts = {
+			smear_between_buffers = true,
+
+			-- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+			smear_between_neighbor_lines = true,
+
+			-- Draw the smear in buffer space instead of screen space when scrolling
+			scroll_buffer_space = true,
+
+			-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+			-- Smears and particles will look a lot less blocky.
+			legacy_computing_symbols_support = true,
+
+			-- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+			smear_insert_mode = false,
+		},
+	},
 })
 
 -- ========================================================================== --
